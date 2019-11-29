3af8c2e3adb25c4755d9442b10fd1b0eb8843f7d
joao-zanetti <57362590+joao-zanetti@users.noreply.github.com>
2019-11-29 16:05:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.database().ref('devicetokens').push(original);
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
+
+ exports.changeValue = functions.database.ref('devicetokens/{pushId}')
+    .onCreate((snapshot, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = snapshot.val();
+      const newValue = 'blablabla';
+      
+      // Setting an other value sibling in the Realtime Database returns a Promise.
+      return snapshot.ref.set({devtoken:uppercase});
+    });
